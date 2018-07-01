04fdc77267aa2e7d1806143004ac15efb4c64168
majd-kho <kho.majds@gmail.com>
2018-07-01 15:07:34 -0400

    introduced bootstrap styling, inline styling, in addition to template vs templateURL and selector attribute vs element vs class

--- angular/my-angular-app/src/app/app.component.ts
+++ angular/my-angular-app/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // you can use also inline templating"template: <h1>blabla</h1>"
+  // the same goes for style but they have to be in an array
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
